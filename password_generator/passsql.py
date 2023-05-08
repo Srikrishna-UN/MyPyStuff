71c016f15201bb2d47e061ab30c426354ba046a4
Srikrishna-UN <srikrishnaun2004@gmail.com>
2023-05-08 23:52:23 +0530

    Commit

--- password_generator/passsql.py
+++ password_generator/passsql.py
@@ -1,9 +1,9 @@
-import
-
-password_db=mysql.connector.connect(
-    host='localhost',
-    user='root',
-    password='$Knavada'
-)
-
-
+# import mysql-connector-python
+#
+# password_db=mysql.connector.connect(
+#     host='localhost',
+#     user='root',
+#     password='$Knavada'
+# )
+#
+#
