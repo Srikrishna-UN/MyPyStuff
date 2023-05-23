995a51e9872249eec1dc07b7055280607d9ade69
Srikrishna-UN <srikrishnaun2004@gmail.com>
2023-05-23 22:24:14 +0530

    Added some logic to the file , needs work

--- Logic_probs/prison.py
+++ Logic_probs/prison.py
@@ -7,4 +7,39 @@ In Round 4, every fourth door (4, 8, 12..), if Door is open,, he closes it. If D
 He does this for 100 Rounds. At the end, who are the lucky prisoners ?
 b) Prepare 2 letters. Letter1 to PM giving list of lucky prisoners and release date as today. Letter2 to Jailer giving list of unlucky prisoners who will be released after 4 weeks.
 """
+# Initialization
+prison_doors = []
+open_doors = []
+for _ in range(100):
+    prison_doors.append(0)
+
+
+def prison_iters(num):
+    if num == 0:
+        for i in range(num, 100):
+            if prison_doors[i] == 1:
+                prison_doors[i] = 0
+            elif prison_doors[i] == 0:
+                prison_doors[i] = 1
+    else:
+        for i in range(num, 100, num):
+            if prison_doors[i] == 1:
+                prison_doors[i] = 0
+            elif prison_doors[i] == 0:
+                prison_doors[i] = 1
+    # for _ in prison_doors:
+    #     if _ == 1:
+    #         open_doors.append(prison_doors.index(_))
+
+
+for z in range(100):
+    prison_iters(z)
+
+print(prison_doors)
+
+for _ in prison_doors:
+    if _ == 1:
+        open_doors.append(prison_doors.index(_))
+
+# print(open_doors)
 
